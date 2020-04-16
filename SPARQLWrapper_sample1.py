c5463922f0d93a0f1bf1892ab7631e857b93e9e8
KeitaK63 <keita.kawano556@mail.kyutech.jp>
2020-04-16 23:24:37 +0900

    0416

---
+++ SPARQLWrapper_sample1.py
@@ -0,0 +1,12 @@
+# https://hironsan.hatenablog.com/entry/python-dbpedia-sparql から引用
+from SPARQLWrapper import SPARQLWrapper
+
+sparql = SPARQLWrapper(endpoint='http://ja.dbpedia.org/sparql', returnFormat='json')
+sparql.setQuery("""
+    PREFIX dbpedia-owl:  <http://dbpedia.org/ontology/>
+    SELECT ?pref WHERE {
+    ?pref dbpedia-owl:wikiPageWikiLink category-ja:日本の都道府県.  }
+""")
+results = sparql.query().convert()
+
+print(results["results"]["bindings"])
